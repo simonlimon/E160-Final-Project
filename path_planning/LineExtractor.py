c026055c07fb519233fb70d08de7e33bf85d727e
simonlimon <simonpfish@gmail.com>
2018-04-13 18:26:32 -0700

    better hough lines transform. still has to be tweaked

--- path_planning/LineExtractor.py
+++ path_planning/LineExtractor.py
@@ -1,26 +1,81 @@
-import cv2
+"""
+@file hough_lines.py
+@brief This program demonstrates line finding with the Hough transform
+https://docs.opencv.org/3.4.0/d9/db0/tutorial_hough_lines.html
+"""
+import sys
+import math
+import cv2 as cv
 import numpy as np
 
-def extract_lines(filename):
-    img = cv2.imread(filename)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    edges = cv2.Canny(gray,50,150,apertureSize = 3)
+def main(argv):
+    
+    default_file =  "../../../../data/sudoku.png"
+    filename = argv[0] if len(argv) > 0 else default_file
+    # Loads an image
+    src = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        print ('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
+        return -1
+    
+    dst = cv.Canny(src, 50, 200, None, 3)
+    
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+    
+    lines = cv.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
+    
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+    
+    
+    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 100, 50)
+    
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
+    
+    cv.imshow("Source", dst)
+    cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+    
+    cv.waitKey()
+    return 0
 
-    lines = cv2.HoughLines(edges,1,np.pi/180,200)
-    for rho,theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+def skeletonize(img):
+    """ OpenCV function to return a skeletonized version of img, a Mat object"""
 
-        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+    #  hat tip to http://felix.abecassis.me/2011/09/opencv-morphological-skeleton/
 
-    cv2.imwrite('houghlines3.jpg',img)
+    img = img.copy() # don't clobber original
+    skel = img.copy()
 
+    skel[:,:] = 0
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
 
+    while True:
+        eroded = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel)
+        temp = cv2.morphologyEx(eroded, cv2.MORPH_DILATE, kernel)
+        temp  = cv2.subtract(img, temp)
+        skel = cv2.bitwise_or(skel, temp)
+        img[:,:] = eroded[:,:]
+        if cv2.countNonZero(img) == 0:
+            break
+
+    return skel
+    
 if __name__ == "__main__":
-    extract_lines('./maze_photos/test_img.png')
+    main(sys.argv[1:])
